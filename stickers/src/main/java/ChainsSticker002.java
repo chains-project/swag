708d43751beeaa641a7d984c39ca969d7fea6f93
bbaudry <baudry@kth.se>
2023-03-19 14:59:11 +0100

    gen art with jenkins sbom and CHAINS

--- stickers/src/main/java/ChainsSticker002.java
+++ stickers/src/main/java/ChainsSticker002.java
@@ -12,12 +12,18 @@ import com.google.gson.JsonParser;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonArray;
 
+/* reads all sha-256 checksums from a CycloneDX SBOM to generate an artwork
+ * the CycloneDX SBOM is passed in the variable fileName
+ */
+
 public class ChainsSticker002 extends PApplet {
+
+    String fileName = "bomJenkins.json";
     int shalen = 64; // length of a sha-256 in hex
     int bytelen = 16; // number of values in hex
-    int offset = 176;
-    int w = shalen * (bytelen + 2) + offset; // canvas is square, we add to bytelen to allow for 1 px space on the side
-                                             // of each element that represents one character in the sha
+    int offset = 276; // margin around the image
+    // canvas is square, we add to bytelen to allow for 1 px space on the side of each element that represents one character in the sha
+    int w = shalen * (bytelen + 2) + offset; 
     int h = w;
     float cx = w / 2;
     float cy = h / 2;
@@ -27,11 +33,9 @@ public class ChainsSticker002 extends PApplet {
     Gson gson = new Gson();
     ArrayList<String> hashes = new ArrayList<>();
     int hashes_ind = 0;
+    String sha;
     float angle = 0;
     double angle_step;
-    String sha;
-    float x1 = -(w / 2 - 88);
-    float x2 = w / 2 - 88;
 
     @Override
     public void settings() {
@@ -41,83 +45,56 @@ public class ChainsSticker002 extends PApplet {
     @Override
     public void setup() {
         colorMode(HSB, 360, 100, 100);
-        initMapHex();
         background(0, 0, 0);
-        //hashes.add("21af30c92267bd6122c0e0b4d20cccb6641a37eaf956c6540ec471d584e64a7b");
-        //hashes.add("21af39c92267bd6122c0e0b4d23cdcb6641a37eaf956c6540ec471d584e64a7b");
-        //hashes.add("21af39c92267bd6142c0e0b4d23cdcb6641a3beaf956c6540ec471df84e64a7b");
+        initMapHex();
         get_shas();
-        angle_step = 1.66;//180/hashes.size()+1;
-        System.out.println(hashes.size()+" "+angle_step);
+        angle_step = 180.0 / hashes.size();
     }
 
-    private void get_shas(){
-        try {
-            FileReader fr = new FileReader("bomJenkins.json");
-            BufferedReader br = new BufferedReader(fr);
-            //convert the json string back to object
-            JsonElement sbom = JsonParser.parseReader(br);
-            JsonObject sbomObj = sbom.getAsJsonObject();
-            JsonArray components = sbomObj.get("components").getAsJsonArray();
-            for (JsonElement c : components) {
-                if (c.getAsJsonObject().get("hashes")!=null){
-                    JsonArray hashcollection = c.getAsJsonObject().get("hashes").getAsJsonArray();
-                    for (JsonElement h : hashcollection) {
-                        if (h.getAsJsonObject().get("alg").getAsString().equals("SHA-256")){
-                            System.out.println(h.getAsJsonObject().get("content").getAsString());
-                            hashes.add(h.getAsJsonObject().get("content").getAsString());
-                        }
-                    }
-                }
-            }
-              
-            //System.out.println( sbomObj.get("components").getAsJsonArray().get(2).getAsJsonObject().get("hashes").getAsJsonArray().get(0));    
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    
-    }
 
     @Override
     public void draw() {
-        drawshas();
-        /*
-         * john("CHAINS", 131, false);
-         * rad += 80;
-         * john("https://chains.proj.kth.se", 62, true);
-         * save("chains-sticker002.png");
-         */
-        // noLoop();
+        if (hashes_ind < hashes.size()) {
+            drawshas();
+            hashes_ind++;
+        }
+        else{
+            noLoop();
+            john("CHAINS", 191, false); //131
+            rad += 80;
+            john("https://chains.proj.kth.se", 62, true);
+            save(fileName+".png");
+        }
+         
     }
 
     private void drawshas() {
         translate(w / 2, h / 2);
+        noFill();
         stroke(0, 0, 100);
-        strokeWeight(3);
-        if (hashes_ind < hashes.size()) {
-            sha=hashes.get(hashes_ind);
-            if (sha.length() == shalen) {
-                int step = bytelen + 2;
-                float x = -rad / 2;
-                float y = h / 2;
-                int local;
-                float off;
-                rotate(radians(angle));
-                for (int i = 0; i < sha.length(); i++) {
-                    local = maphex.get(sha.charAt(i));
-                    if (local == 15) {
-                        stroke(0, 100, 100);
-                    } else {
-                        stroke(0, 0, 100);
-                    }
-                    off = (step - local) / 2;
-                    line((x + 1 + off), 0, (x + 1 + +off + local), 0);
-                    x += step;
+        strokeWeight(5);
+        sha = hashes.get(hashes_ind);
+        if (sha.length() == shalen) {
+            int step = bytelen + 2;
+            float x = -rad / 2;
+            float y = h / 2;
+            int local;
+            float off;
+            rotate(radians(angle));
+            for (int i = 0; i < sha.length(); i++) {
+                local = maphex.get(sha.charAt(i));
+                if (local == 15) {
+                    stroke(0, 100, 100);
+                } else {
+                    stroke(0, 0, 100);
                 }
-                angle+=angle_step;
-                System.out.println("::: angle: " + angle + "; hashes_ind: " + hashes_ind);
+                off = (step - local) / 2;
+                //line((x + 1 + off), 0, (x + 1 + +off + local), 0);
+                ellipse(x+step/2,0,local,local);
+                x += step;
             }
-            hashes_ind++;
+            angle += angle_step;
+            System.out.println("::: angle: " + angle + "; hashes_ind: " + hashes_ind);
         }
     }
 
@@ -140,6 +117,32 @@ public class ChainsSticker002 extends PApplet {
         maphex.put('f', 15);
     }
 
+    /* finds all sha-256 checksums in fileName and stores them in the hashes Array */
+    private void get_shas() {
+        try {
+            FileReader fr = new FileReader(fileName);
+            BufferedReader br = new BufferedReader(fr);
+            JsonElement sbom = JsonParser.parseReader(br);
+            JsonObject sbomObj = sbom.getAsJsonObject();
+            JsonArray components = sbomObj.get("components").getAsJsonArray();
+            for (JsonElement c : components) {
+                if (c.getAsJsonObject().get("hashes") != null) {
+                    JsonArray hashcollection = c.getAsJsonObject().get("hashes").getAsJsonArray();
+                    for (JsonElement h : hashcollection) {
+                        if (h.getAsJsonObject().get("alg").getAsString().equals("SHA-256")) {
+                            System.out.println(h.getAsJsonObject().get("content").getAsString());
+                            hashes.add(h.getAsJsonObject().get("content").getAsString());
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
     // https://processing.org/tutorials/text/#displaying-text-character-by-character
     private void john(String baldessari, int art, boolean opposite) {
         PFont f = createFont("FreeMono", art, true);// DejaVuSansMono
@@ -149,6 +152,7 @@ public class ChainsSticker002 extends PApplet {
         smooth();
         noFill();
         stroke(0, 0, 100);
+        strokeWeight(17);
         // We must keep track of our position along the curve
         float arclength = 0;
         // For every box
@@ -156,7 +160,6 @@ public class ChainsSticker002 extends PApplet {
             // Instead of a constant width, we check the width of each character.
             char currentChar = baldessari.charAt(i);
             float tw = textWidth(currentChar) * 4;
-
             // Each box is centered so we move half the width
             arclength += tw / 2;
             // Angle in radians is the arclength divided by the radius

567dc496967edd235fe3c66f66399f45051307ac
bbaudry <baudry@kth.se>
2023-03-22 13:50:33 +0100

    3 variants for a checksum sticker

--- stickers/src/main/java/ChainsSticker002.java
+++ stickers/src/main/java/ChainsSticker002.java
@@ -21,7 +21,7 @@ public class ChainsSticker002 extends PApplet {
     String fileName = "./stickers/src/main/resources/bomJenkins.json";
     int resolution = 2;
     int thickness = 5;
-    String shape = "square"; // can take 3 values: "square", "circle", "line"
+    String shape = "line"; // can take 3 values: "square", "circle", "line"
     int shalen = 64; // length of a sha-256 in hex
     int bytelen = 16; // number of values in hex
     int offset = 276; // margin around the image
